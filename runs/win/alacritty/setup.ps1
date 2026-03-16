cd58635be2ed3142aa24841767a762c09197d2ff
Federico Varano <f.varano@reply.it>
2026-03-16 02:04:49 +0100

    fix alacritty setup

--- runs/win/alacritty/setup.ps1
+++ runs/win/alacritty/setup.ps1
@@ -9,4 +9,9 @@ Write-Host "XDG_CONFIG_HOME is set to $env:XDG_CONFIG_HOME" -ForegroundColor Gre
 # copy env\.config\alacritty to XDG_CONFIG_HOME
 Copy-Item -Path ".\env\.config\alacritty" -Destination $env:XDG_CONFIG_HOME -Recurse -Force
 
+# create symlink from AppData\Roaming\alacritty to $XDG_CONFIG_HOME\alacritty if doesn't exist
+if (-not (Test-Path "$env:USERPROFILE\AppData\Roaming\alacritty")) {
+    New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\alacritty" -Target "$env:XDG_CONFIG_HOME\alacritty"
+}
+
 Write-Host "Alacritty setup complete!" -ForegroundColor Green
