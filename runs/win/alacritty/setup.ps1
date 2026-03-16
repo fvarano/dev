109f7af1c17cddcdba5d919b696205cb1dd9d3a1
Federico Varano <f.varano@reply.it>
2026-03-16 02:34:31 +0100

    fix alacritty dummy file contents

--- runs/win/alacritty/setup.ps1
+++ runs/win/alacritty/setup.ps1
@@ -9,10 +9,15 @@ Write-Host "XDG_CONFIG_HOME is set to $env:XDG_CONFIG_HOME" -ForegroundColor Gre
 # copy env\.config\alacritty to XDG_CONFIG_HOME
 Copy-Item -Path ".\env\.config\alacritty" -Destination $env:XDG_CONFIG_HOME -Recurse -Force
 
-# create a dummy alacritty.toml in AppData\Roaming\alacritty that imports the Windows config
-if (-not (Test-Path "$env:USERPROFILE\AppData\Roaming\alacritty\alacritty.toml")) {
-    $content = "[general]\nimport = [\"~/.config/alacritty/alacritty-windows.toml\"]"
-    Set-Content -Path "$env:USERPROFILE\AppData\Roaming\alacritty\alacritty.toml" -Value $content
+$windows_alacritty_path = "$env:USERPROFILE\AppData\Roaming\alacritty"
+
+# create alacritty directory if it doesn't exist
+if (-not (Test-Path $windows_alacritty_path)) {
+    New-Item -ItemType Directory -Path $windows_alacritty_path
 }
+
+# create a dummy alacritty.toml in $windows_alacritty_path that imports the Windows config
+$content = "[general]`nimport = [`"~/.config/alacritty/alacritty-windows.toml`"]"
+Set-Content -Path "$windows_alacritty_path\alacritty.toml" -Value $content
 
 Write-Host "Alacritty setup complete!" -ForegroundColor Green
