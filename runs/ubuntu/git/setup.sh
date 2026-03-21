475dc7d87b4ac3cfe4f258d0dffbc55499f7fe1b
Federico Varano <f.varano@reply.it>
2026-03-21 13:37:46 +0100

    feat: ubuntu setup initialized

---
+++ runs/ubuntu/git/setup.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Federico Varano"
+git config --global user.email "f.varano@reply.it"
+
+git config --global pull.rebase true
+git config --global rebase.autoStash true
